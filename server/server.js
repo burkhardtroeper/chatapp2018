839a1c8094773d63f9bec143234890e6dd440613
Axelschweiss <>
2018-06-01 12:57:42 +0200

    first socketio tests

--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
+
+const socketIO = require('socket.io');
 
 const express = require('express');
 
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+
+});
+
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
-})
-
-
-
-
+});
